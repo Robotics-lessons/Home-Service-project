b993d8cc58b757d8b0598fa2675d28cd92fd3cc1
yangnie <yang.nie@gmail.com>
2018-05-22 05:37:46 -0400

    feat: modify test_slam.sh

--- shellscripts/test_slam.sh
+++ shellscripts/test_slam.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 ##source /opt/ros/kinetic/setup.bash
 
-roslaunch homeservice homeservice.launch world_file:=$HOME/catkin_ws/src/homeservice/world/homeservice.world
+xterm -e " roslaunch homeservice homeservice.launch world_file:=$HOME/catkin_ws/src/homeservice/world/homeservice.world " &
+sleep 5
+xterm -e " roslaunch homeservice gmapping.launch " &
 #sleep 5
-# roslaunch turtlebot_gazebo turtlebot_world.launch
-#sleep 5
+xterm -e " roslaunch turtlebot_teleop keyboard_teleop.launch " 
+
