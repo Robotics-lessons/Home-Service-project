e075d63de4490a0546e27ad5290b80e322f3e400
yangnie <yang.nie@gmail.com>
2018-05-21 22:21:26 -0400

    docs: first init

---
+++ shellscripts/test_slam.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+##source /opt/ros/kinetic/setup.bash
+
+roslaunch homeservice homeservice.launch world_file:=$HOME/catkin_ws/src/homeservice/world/homeservice.world
+#sleep 5
+# roslaunch turtlebot_gazebo turtlebot_world.launch
+#sleep 5
