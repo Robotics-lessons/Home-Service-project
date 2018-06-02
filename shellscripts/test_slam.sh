b0856ed647843a24c1098a5e5c29bdc82b97209e
yangnie <yang.nie@gmail.com>
2018-06-02 15:04:50 -0400

    feat: fix script files

--- shellscripts/test_slam.sh
+++ shellscripts/test_slam.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 ##source /opt/ros/kinetic/setup.bash
-<<<<<<< HEAD
 currentPath=$(pwd)
 echo $currentPath
 xterm -e " roslaunch homeservice homeservice.launch world_file:=$currentPath/../homeservice/world/homeservice.world " &
-=======
-
-xterm -e " roslaunch homeservice homeservice.launch world_file:=$HOME/catkin_ws/src/homeservice/world/homeservice.world " &
->>>>>>> 153ab6a3524a442855bc99171e824e74850dabc9
 sleep 5
 xterm -e " roslaunch homeservice gmapping.launch " &
 #sleep 5
